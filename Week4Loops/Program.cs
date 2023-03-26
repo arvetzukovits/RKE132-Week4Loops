44d4d3b6b22dd8b7d476c9fda43e119c2fdd4375
arvetzukovits <arvet.zukovitsrtktk.ee>
2023-03-26 19:21:41 +0300

    add week4loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do not repeat yourself
+
+for(int i = 0; i < 5; i++)// i++ = i+1
+{
+    Console.WriteLine("We don't need no educations.");
+
+}
+
+Console.WriteLine("Have a nice day!");
